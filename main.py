443651cafdfed4b5f8e719a041345dfe9fe8e041
TijlB99 <info@tijlb.eu>
2022-04-07 19:05:51 +0200

    ch

--- main.py
+++ main.py
@@ -3,9 +3,12 @@ from routePlotter import plot
 kmlPath = "../Records.json"
 be = (2.4, 6.6, 49.4, 51.7)
 zoombe = 9
+ch = (5.4,11.0,45.5,47.8)
+zoomch = 9
 world = (-179, 79, -89, 89)
 zoomworld = 1
 
 # plot(kmlPath, boundingBox=be, zoom=zoombe, verbose=True)
+plot(kmlPath, boundingBox=ch, zoom=zoomch, verbose=True)
 # plot(kmlPath, boundingBox=world, zoom=zoomworld, verbose=True)
-plot(kmlPath, verbose=True)
+# plot(kmlPath, verbose=True)
